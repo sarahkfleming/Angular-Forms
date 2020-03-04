b0f9d7788de9cd579b2589e6e9eec4876077d8af
Sarah Fleming <JJ10334@tn.gov>
2020-03-04 11:13:25 -0600

    setting up data service and subscribing to observable

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserSettings } from '../data/user-settings';
-import { NgForm } from '@angular/forms';
+import { NgForm, NgModel } from '@angular/forms';
+import { DataService } from '../data/data.service';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -20,13 +21,20 @@ export class UserSettingsFormComponent implements OnInit {
   // Use the spread operator to make a copy of this flat object before displaying it to the user to protect the original data
   userSettings: UserSettings = { ...this.originalUserSettings };
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
   }
 
   onSubmit(form: NgForm) {
     console.log('in onSubmit: ', form.valid);
+    this.dataService.postUserSettingsForm(this.userSettings).subscribe(result => console.log('success: ', result),
+    error => console.log('error: ', error));
+  }
+
+  onBlur(field: NgModel) {
+    console.log('in onBlur: ', field.valid);
+
   }
 
 }
